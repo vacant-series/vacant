7c141569c341905ae8052a4416663af3cbaff2e0
cbb67 <cbb67@cornell.edu>
2020-06-03 01:04:39 -0400

    tried to change preview camera.py

--- ObjectDetection/camera.py
+++ ObjectDetection/camera.py
@@ -3,7 +3,7 @@ from time import sleep
 
 camera = PiCamera()
 
-camera.start_preview()
+camera.start_preview(fullscreen=False,window=(100,200,300,400))
 sleep(5)
 camera.capture('/home/pi/Desktop/cynzim.jpg')
 camera.stop_preview()
