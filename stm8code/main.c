fedc5b2e20c82bc3a17d1e096506f4ef41267089
nhz2 <nhz2@cornell.edu>
2020-06-21 01:58:12 -0400

    main

--- stm8code/main.c
+++ stm8code/main.c
@@ -1,5 +1,25 @@
 #include "stm8l.h"
 #include "pins.h"
+const unsigned char numberlut[]={0b1111110,//0
+							0b0110000,//1
+							0b1101101,//2
+							0b1111001,//3
+							0b0110011,//4
+							0b1011011,//5
+							0b1011111,//6
+							0b1110000,//7
+							0b1111111,//8
+							0b1111011};//9
+
+//pin definitions
+const unsigned char digit2[]= {};
+const unsigned char digit3[]= {};
+const unsigned char digit4[]= {};
+const unsigned char bc1= ;
+const unsigned char bp= ;
+const unsigned char col= ;
+const unsigned char alarm= ;
+
 
 int main() {
 	// Configure pins
