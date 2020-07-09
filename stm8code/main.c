d3c29031c38a515b934b38c771f309b533f53295
nhz2 <nhz2@cornell.edu>
2020-07-08 20:47:56 -0400

    pin num changes

--- stm8code/main.c
+++ stm8code/main.c
@@ -12,13 +12,13 @@ const unsigned char numberlut[]={0b1111110,//0
 							0b1111111,//8
 							0b1111011};//9
 
-//pin definitions
-const unsigned char digit2[]= {27,25,16,9,2,29,31};
-const unsigned char digit3[]= {24,22,10,7,14,28,30};
-const unsigned char digit4[]= {8,17,19,21,23,18,20};
-const unsigned char bc1= 13;
-const unsigned char bp= 15;
-const unsigned char col= 32;
+//pin definitions				a,b,c,d,e,f,g
+const unsigned char digit2[]= {31,30,13,10,9,32,2};
+const unsigned char digit3[]= {25,24,16,15,14,27,28};
+const unsigned char digit4[]= {21,20,19,18,17,22,23};
+const unsigned char bc1= 7;
+const unsigned char bp= 8;
+const unsigned char col= 29;
 const unsigned char alarm= 3;
 
 unsigned char digit1num;
@@ -65,7 +65,7 @@ void inctimer(){
 		       digit2num++;
 		       if(digit2num==10){
 			       digit2num=0;
-			       digit1num=++;
+			       digit1num++;
 			       if(digit1num=2){
 				       digit1num=1;
 				       digit2num=9;
