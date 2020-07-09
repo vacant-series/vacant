7056ee2f16a570d8899d9128a168f36cf6d992a8
nhz2 <nhz2@cornell.edu>
2020-07-09 02:47:28 -0400

    calibrated with seconds at start

--- stm8code/main.c
+++ stm8code/main.c
@@ -10,7 +10,8 @@ const unsigned char numberlut[]={0b1111110,//0
 							0b1011111,//6
 							0b1110000,//7
 							0b1111111,//8
-							0b1111011};//9
+							0b1111011,//9
+							0b0000000};//10 aka off
 
 //pin definitions				a,b,c,d,e,f,g
 const unsigned char digit2[]= {31,30,13,10,9,32,2};
@@ -21,20 +22,32 @@ const unsigned char bp= 8;
 const unsigned char col= 29;
 const unsigned char alarm= 3;
 
+unsigned char secondsL;
+unsigned char secondsH;
+unsigned char minutesL;
+unsigned char minutesH;
+unsigned char hours;
 unsigned char digit1num;
 unsigned char digit2num;
 unsigned char digit3num;
 unsigned char digit4num;
+unsigned char column;
 unsigned char alarmcount=0;
 
 /** initializes output regs to display zero*/
 void inittimer(){
+	secondsL=0;
+	secondsH=0;
+	minutesL=0;
+	minutesH=0;
+	hours=0;
 	digit1num=0;
-	digit2num=0;
+	digit2num=10;
 	digit3num=0;
 	digit4num=0;
+	column=0;
 	clearAll();
-	pinSet(col);
+	//pinSet(col);
 	//now set some lcd pins
 	//display digit 4
 	unsigned char zcode= numberlut[0];
@@ -48,11 +61,6 @@ void inittimer(){
     	unsigned char bitnum= pinnum2bitnum(digit3[i]);
     	*portnumODR(portnum) |= (zcode>>(6-i)&1)<<bitnum;
 	}
-	for(int i=0; i<7; i++){
-		unsigned char portnum= pinnum2portnum(digit2[i]);
-    	unsigned char bitnum= pinnum2bitnum(digit2[i]);
-    	*portnumODR(portnum) |= (zcode>>(6-i)&1)<<bitnum;
-	}
 }
 
 void inctimer(){
@@ -60,27 +68,56 @@ void inctimer(){
 	unsigned char old3= digit3num;
 	unsigned char old2= digit2num;
 	unsigned char old1= digit1num;
-	digit4num++;
-	if(digit4num==10){
-	       digit4num=0;
-	       digit3num++;
-	       if(digit3num==6){
-		       digit3num=0;
-		       digit2num++;
-		       if(digit2num==10){
-			       digit2num=0;
-			       if(digit1num==1){
-				       digit1num=1;
-				       digit2num=9;
-				       digit3num=5;
-				       digit4num=9;
-			       }
-			       if(digit1num==0){
-				   digit1num=1;
+	unsigned char oldcolumn= column;
+
+	secondsL++;
+	if(secondsL==10){
+	       secondsL=0;
+	       secondsH++;
+	       if(secondsH==6){
+		       secondsH=0;
+		       minutesL++;
+		       if(minutesL==10){
+			       minutesL=0;
+			       minutesH++;
+			       if(minutesH==6){
+			       		minutesH=0;
+					hours++;
+					if (hours==20){
+						hours=19;
+						minutesH=5;
+						minutesL=9;
+						secondsH=5;
+						secondsL=9;
+					}
 			       }
 		       }
 	       }
 	}
+	
+	if(column==0 && minutesL==1){
+		//move to minutes mode.
+		column=1;
+	}
+	if(column==0){
+		//seconds mode
+		digit4num=secondsL;
+		digit3num=secondsH;
+	} else {
+		//minutes hours mode
+		digit4num= minutesL;
+		digit3num= minutesH;
+		if (hours<10){
+			digit2num=hours;
+		} else {
+			digit1num=1;
+			digit2num= hours-10;
+		}
+	}
+		
+
+	
+	if(oldcolumn!=column) pinToggle(col);
 	if(old1!=digit1num) pinToggle(bc1);
 	//now toggle some lcd pins
 	//display digit 4
@@ -146,7 +183,7 @@ int main() {
 	inittimer();
 	// Loop
 	do {
-		for(int i=0; i<10; i++){
+		for(int i=0; i<82; i++){
 			if(1==pinRead(alarm)){
 				alarmcount= 0;	
 			}else{
