578fd84cf966f173ba4c6d9c3e10c59b2ffab5ba
BediakoCynoc <boahenecynoc@gmail.com>
2020-07-11 15:34:18 -0400

    added readme and upated camera.py to use conf.json

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # vacant
 Check if a room is unoccupied.
+Our JSON configuration filelet’s take a look at our conf.json  file:
+
+vacant with the Raspberry Pi, Python, and OpenCV
+{
+	"delta_thresh": 20,
+	"resolution": [
+		640,
+		480
+	],
+	"detection_speed":10,
+	"fps": 16,
+	"min_area": 500,
+	"timer_pin":23
+}
+This JSON configuration file stores a bunch of important variables. Let’s look at each of them:
+delta_thresh : The minisum absolute value difference between our current frame and averaged frame for a given pixel to be “triggered” as motion. Smaller values will lead to more motion being detected, larger values to less motion detected.
+resolution : The width and height of the video frame from our Raspberry Pi camera.
+fps : The desired Frames Per Second from our Raspberry Pi camera.
+min_area : The minimum area size of an image (in pixels) for a region to be considered motion or not. Smaller values will lead to more areas marked as motion, whereas higher values of min_area  will only mark larger regions as motion.
+timer_pin : The pin number to cnnect the timer to
+detection_speed: The rate at which images are taken to comapare with the average image of the room. Larger values would lead to slower detection rate
+
+command to run  the sript: python3 /home/pi/vacant/ObjectDetection/camera.py --conf /home/pi/vacant/ObjectDetection/conf.json 
---
+++ camera.py
@@ -0,0 +1,94 @@
+import picamera 
+from picamera.array import PiRGBArray
+import imutils
+from time import sleep
+import cv2
+import warnings
+from gpiozero import MotionSensor
+import  RPi.GPIO as GPIO
+warnings.filterwarnings("ignore")
+GPIO.setmode(GPIO.BCM)
+   
+class Vacant():
+    def __init__(self):
+        #self.ms  = MotionSensor(15, pull_up=True,queue_len=20)
+        self.avg = None
+        self.camera = picamera.PiCamera()
+        self.camera.resolution = (640,480)
+        self.camera.framerate = 16
+        self.rawCapture = PiRGBArray(self.camera, size=(640,480))
+        self.timerpin=23
+        GPIO.setup(self.timerpin,GPIO.IN)
+        self.detection_speed = 10
+     
+    def human_detector(self,gray):      
+        # accumulate the weighted average between the current frame and
+        # previous frames, then compute the difference between the current
+        # frame and running average
+        frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(self.avg))        
+        # threshold the delta image, dilate the thresholded image to fill
+        # in holes, then find contours on thresholded image
+        thresh = cv2.threshold(frameDelta, 20, 255,
+            cv2.THRESH_BINARY)[1]
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)  # loop over the contours
+        num_of_min_cnts = 0        
+        for c in cnts:
+            # if the contour is too small, ignore it
+            if cv2.contourArea(c) < 5000:
+                num_of_min_cnts+=1
+                continue
+            self.hold_clock()
+            print("[INFO] warming up...")
+            
+    def room_averager(self):
+        # initialize the camera and grab a reference to the raw camera capture
+        print("[INFO] warming up...")
+        sleep(2.5)
+        count = 0
+        # capture frames from the camera
+        for f in self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True):
+            # grab the raw NumPy array representing the image and initialize
+            # the timestamp and occupied/unoccupied text
+            self.reset_clock()
+            frame = f.array
+            # resize the frame, convert it to grayscale, and blur it
+            frame = imutils.resize(frame, width=500)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (21, 21), 0)
+            count +=1
+
+            # if the average frame is None, initialize it
+            if self.avg is None:
+                print("[INFO] starting background model...")
+                self.avg = gray.copy().astype("float")
+                self.rawCapture.truncate(0)
+                continue
+            # accumulate the weighted average between the current frame and
+            # previous frames
+            cv2.accumulateWeighted(gray, self.avg, 0.5)
+            # clear the stream in preparation for the next frame
+            self.rawCapture.truncate(0)
+            if count == self.detection_speed:
+                count = 0
+                self.human_detector(gray)
+                
+    def reset_clock(self):
+        GPIO.setup(self.timerpin,GPIO.IN)
+          
+    def hold_clock(self):
+        GPIO.setup(self.timerpin,GPIO.OUT)
+    
+def main():
+    vacant = Vacant()
+    vacant.room_averager()
+    
+if __name__ == "__main__":
+    main()
+    
+
+        
+
+
